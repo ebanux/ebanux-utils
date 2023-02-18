be54972b986f8bfbdb012138ec1d722ea6830a1d
yoandrypa <yoandrypa@gmail.com>
2023-02-18 01:23:48 -0500

    Update Messaging class

--- src/messaging.js
+++ src/messaging.js
@@ -10,17 +10,17 @@ class Messaging {
     return `${senderId || 'Global'}/${messageId}`;
   }
 
-  addMessagingListener(messageId, callBack, senderId) {
+  addListener(messageId, callBack, senderId) {
     const eventType = this.getEventType(messageId, senderId);
     const subscription = emitter.addListener(eventType, callBack);
     this.subscriptions.push(subscription);
     return subscription;
   }
 
-  setMessagingListener(messageId, callBack, senderId) {
+  setListener(messageId, callBack, senderId) {
     const eventType = this.getEventType(messageId, senderId);
 
-    this.subscriptions.forEach((s) => (s.eventType === eventType) && this.delMessagingListener(s));
+    this.subscriptions.forEach((s) => (s.eventType === eventType) && this.delListener(s));
 
     return emitter.addListener(eventType, callBack);
   }
@@ -36,12 +36,30 @@ class Messaging {
     }
   }
 
-  delMessagingListener(subscriptions) {
+  delListener(subscriptions) {
     subscriptions = Array.isArray(subscriptions) ? subscriptions : [subscriptions];
     subscriptions.forEach((dS) => {
       this.subscriptions = this.subscriptions.filter((cS) => cS !== dS);
       dS.remove();
     });
+  }
+
+  // TODO: Deprecated
+  addMessagingListener(messageId, callBack, senderId) {
+    console.warn('The addMessagingListener property is deprecated, use addListener instead.');
+    this.addListener(messageId, callBack, senderId);
+  }
+
+  // TODO: Deprecated
+  emitMessage(messageId, data, senderId, timeout) {
+    console.warn('The emitMessage property is deprecated, use emit instead.');
+    this.emit(messageId, data, senderId, timeout);
+  }
+
+  // TODO: Deprecated
+  delMessagingListener(subscriptions) {
+    console.warn('The delMessagingListener property is deprecated, use delListener instead.');
+    this.delListener(subscriptions);
   }
 }
 
