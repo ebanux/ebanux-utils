c58e1e09cfa5b5aea2e6165703c022500585f42c
yoandrypa <yoandrypa@gmail.com>
2023-02-18 00:45:45 -0500

    Update Messaging class

--- src/messaging.js
+++ src/messaging.js
@@ -4,12 +4,24 @@ const emitter = new EventEmitter();
 
 /* eslint no-param-reassign: ["off"] */
 class Messaging {
+  subscriptions = [];
+
   getEventType(messageId, senderId) {
     return `${senderId || 'Global'}/${messageId}`;
   }
 
   addMessagingListener(messageId, callBack, senderId) {
     const eventType = this.getEventType(messageId, senderId);
+    const subscription = emitter.addListener(eventType, callBack);
+    this.subscriptions.push(subscription);
+    return subscription;
+  }
+
+  setMessagingListener(messageId, callBack, senderId) {
+    const eventType = this.getEventType(messageId, senderId);
+
+    this.subscriptions.forEach((s) => (s.eventType === eventType) && this.delMessagingListener(s));
+
     return emitter.addListener(eventType, callBack);
   }
 
